8d5077a30ddaa9895a082ac9bf1efafd07183f9c
guopeixiong <2569880048@qq.com>
2021-04-11 15:39:08 +0800

    Java作业

--- Java作业/src/test4/Demo.java
+++ Java作业/src/test4/Demo.java
@@ -8,22 +8,16 @@ class OutException extends Exception{
     }
 }
 public class Demo {
-    public static int getIntNumber(Scanner sc){
+    public static int getIntNumber(Scanner sc) throws NumberFormatException{
         System.out.println("请输入成绩:");
         int i=0;
         String str="";
-        try{
-            str=sc.nextLine();
-            i=Integer.parseInt(str);
-        }catch(NumberFormatException e){
-            System.out.println("该成绩不是数字!");
-        }
-        finally {
-            return i;
-        }
+        str=sc.nextLine();
+        i=Integer.parseInt(str);
+        return i;
     }
     public static void isOut(int a) throws OutException{
-        if (a>100){
+        if (a>100||a<0){
             throw new OutException("该成绩不在0~100之间");
         }
     }
@@ -41,9 +35,10 @@ public class Demo {
                 }
             }catch (OutException e) {
                 System.out.println(e.getMessage());
+            }catch (NumberFormatException e){
+                System.out.println("该成绩不是数字");
             }
         }
         System.out.println("成绩总和:"+sum+",平均成绩:"+(double)sum/students);
-        System.out.println(students);
     }
 }
