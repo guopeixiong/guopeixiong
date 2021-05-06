fdd2bdcb988e7974f48a12775113e05c16791358
guopeixiong <2569880048@qq.com>
2021-05-06 22:52:16 +0800

    default

--- Java作业/src/test8/CopyPic.java
+++ Java作业/src/test8/CopyPic.java
@@ -1,4 +1,4 @@
-package text7;
+package test8;
 
 import java.io.*;
 import java.util.Scanner;
@@ -25,41 +25,41 @@ public class CopyPic {
     public int copy(){
         //返回判断后的值后续判断是否进行复制
         int i = this.detect();
-        InputStream in = null;
-        OutputStream out = null;
+        FileReader in = null;
+        FileWriter out = null;
         File src = null;
         File dst = null;
         //判断后地址及文件无误,开始复制
         if (i == 1){
             try {
-                in = new FileInputStream(src = new File(srcPath));
+                in = new FileReader(src = new File(srcPath));
                 //获取复制路径后获取复制的文件名,在复制目的地中被复制的文件文件名与源文件相同但在文件名前加了个(复制版)
-                out = new FileOutputStream(dst = new File(dstPath+"\\"+"(复制版)"+src.getName()));
-                byte[] buffer = new byte[1024];
+                out = new FileWriter(dst = new File(dstPath+"\\"+"(复制版)"+src.getName()));
+                char[] buffer = new char[128];
                 int len;//记录每次读取的字节数,方便后续写入
                 while ((len = in.read(buffer)) != -1){
                     out.write(buffer,0,len);
                 }
                 return 1;//复制结束,返回1
-                }catch (IOException e){
-                    return 0;
-                }finally {
-                    if (in != null){
-                        try {
-                            in.close();
-                        }catch (IOException e){
-                            e.printStackTrace();
-                        }
+            }catch (IOException e){
+                return 0;
+            }finally {
+                if (in != null){
+                    try {
+                        in.close();
+                    }catch (IOException e){
+                        e.printStackTrace();
                     }
-                    if (out != null){
-                        try {
-                            out.close();
-                        }catch(IOException e){
-                            e.printStackTrace();
-                        }
+                }
+                if (out != null){
+                    try {
+                        out.close();
+                    }catch(IOException e){
+                        e.printStackTrace();
                     }
                 }
             }
+        }
         return 0;
     }
     public  String getSrcPath() {
@@ -85,7 +85,7 @@ class Test{
         String src = scanner.nextLine();
         System.out.println("请输入复制目的地址:");
         String dst = scanner.nextLine();
-        CopyPic copy = new CopyPic(src,dst);
+        text7.CopyPic copy = new text7.CopyPic(src,dst);
         System.out.println(copy.copy());
         scanner.close();
     }
