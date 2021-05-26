3841b1617022a8a627a9a7a8351cf7ee2eccb6e1
guopeixiong <2569880048@qq.com>
2021-05-26 13:45:27 +0800

    default

---
+++ Java作业/src/text11/ThreadTest.java
@@ -0,0 +1,44 @@
+package text11;
+
+class Thread1 extends Thread{
+    @Override
+    public void run() {
+        for (int i=0;i<=10;i++){
+            System.out.println("我是线程1");
+            try {
+                Thread.sleep(1000);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
+class Thread2 implements Runnable{
+    @Override
+    public void run() {
+        for (int i=0;i<=10;i++){
+            System.out.println("我是线程2");
+            try {
+                Thread.sleep(1000);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
+public class ThreadTest {
+    public static void main(String[] args) {
+        Thread1 t1=new Thread1();
+        t1.start();
+        Thread t2=new Thread(new Thread2());
+        t2.start();
+        for (int i=0;i<=10;i++){
+            System.out.println("我是主线程");
+            try {
+                Thread.sleep(1000);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
